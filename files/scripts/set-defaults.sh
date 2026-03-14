34897ddd1daabe9d1c4cd738a0b8f2c652770375
Ashi Vered <76535872+AshiVered@users.noreply.github.com>
2026-03-14 19:43:00 +0200

    Fix black screen - try 1

--- files/scripts/set-defaults.sh
+++ files/scripts/set-defaults.sh
@@ -12,10 +12,11 @@ Options=grp:alt_shift_toggle
 Use=true
 EOF
 
-# --- Wallpaper Cleanup ---
+# הגדרת שורת המשימות עם זית, אובסידיאן ו-OnlyOffice
+cat <<EOF > /etc/skel/.config/plasma-org.kde.plasma.desktop-appletsrc
+[SerializationSettings]
+FreeCanary=true
 
-# 1. מחיקת כל תיקיות הרקעים השונות של פדורה ו-KDE (השארת תיקיית F43 בלבד)
-ls -d /usr/share/wallpapers/* | grep -v 'F43' | xargs rm -rf
-
-# 2. בתוך תיקיית F43, מחיקת כל התמונות המקוריות של פדורה והשארת התמונה שלנו בלבד
-find /usr/share/wallpapers/F43/contents/images/ -type f ! -name '3264x2448.jpg' -delete
+[Applets][1][Configuration][General]
+launchers=applications:zayit-Zayit.desktop,applications:onlyoffice-desktopeditors.desktop
+EOF
